d9bc93aeea33a28dfd0b28af2d4197e615e85219
DELL <DELL@DESKTOP-95MSSAI>
2024-03-12 15:52:02 +0530

    Upadate changes

--- IneichenScraper/Program.cs
+++ IneichenScraper/Program.cs
@@ -15,6 +15,7 @@ using System.Text.RegularExpressions;
 using System.Security.Cryptography.X509Certificates;
 using System.Data.SqlClient;
 using System.Data;
+using static IneichenScraper.Program;
 
 namespace IneichenScraper
 {
@@ -125,6 +126,8 @@ namespace IneichenScraper
                 var EndMonth = items.SelectSingleNode(".//div[@class='auction-date-location']/div[1]").InnerText.Trim();
                // Match match_EndMonth = Regex.Match(EndMonth, @"(-)\s?\d{1,2}\s?([a-zA-Z]*)\s?(\d{4})?");
                 Match match_EndMonth = Regex.Match(EndMonth, @"(-)\s?(\d{1,2})\s([a-zA-Z]*)");
+               // Match match_EndMonth = Regex.Match(EndMonth, @"((-)\s?(\d{1,2})\s([a-zA-Z]*))?(CET\s(\d{1,2})\s([a-zA-Z]*))?");
+
                 string matched_EndMonth = match_EndMonth.Groups[3].Value;
 
 
@@ -154,56 +157,150 @@ namespace IneichenScraper
 
 
             }
-            InsertAuctionDataIntoDatabase(IPobj);
+            //InsertAuctionDataIntoDatabase(IPobj);
+            InsertOrUpdateAuctionDataIntoDatabase(IPobj);
 
             Console.ReadLine();
 
 
-            void InsertAuctionDataIntoDatabase(List<IneichenProduct> auctionDataList)
+            //void InsertAuctionDataIntoDatabase(List<IneichenProduct> auctionDataList)
+            //{
+
+            //    string conn = ConfigurationManager.ConnectionStrings["CreateConnection"].ConnectionString;
+
+            //    using (SqlConnection connection = new SqlConnection(conn))
+            //    {
+            //        connection.Open();
+
+            //        try
+            //        {
+            //            foreach (var data in auctionDataList)
+            //            {
+            //                // stored procedure
+            //                string insertQuery = "SP_InsertIneichenProductData";
+
+            //                using (SqlCommand command = new SqlCommand(insertQuery, connection))
+            //                {
+            //                    command.CommandType = CommandType.StoredProcedure;
+
+            //                    command.Parameters.AddWithValue("@Title", data.Title);
+            //                    command.Parameters.AddWithValue("@Description", data.Description);
+            //                    command.Parameters.AddWithValue("@ImageUrl", data.ImageUrl);
+            //                    command.Parameters.AddWithValue("@Link", data.Link);
+            //                    command.Parameters.AddWithValue("@LotCount", data.LotCount);
+            //                    command.Parameters.AddWithValue("@StartDate", data.StartDate);
+            //                    command.Parameters.AddWithValue("@StartMonth", data.StartMonth);
+            //                    command.Parameters.AddWithValue("@StartYear", data.StartYear);
+            //                    command.Parameters.AddWithValue("@StartTime", data.StartTime);
+            //                    command.Parameters.AddWithValue("@EndDate", data.EndDate);
+            //                    command.Parameters.AddWithValue("@EndMonth", data.EndMonth);
+            //                    command.Parameters.AddWithValue("@EndYear", data.EndYear);
+            //                    command.Parameters.AddWithValue("@EndTime", data.EndTime);
+            //                    command.Parameters.AddWithValue("@Location", data.Location);
+
+            //                    command.ExecuteNonQuery();
+            //                }
+            //            }
+            //        }
+            //        catch (Exception ex)
+            //        {
+            //            Console.WriteLine(ex.Message.ToString());
+            //        }
+            //    }
+            //}
+            void InsertOrUpdateAuctionDataIntoDatabase(List<IneichenProduct> auctionDataList)
             {
-               // string conn = request.CreateConnection(ConfigurationManager.ConnectionStrings["DatabaseConnection"].ConnectionString);
                 string conn = ConfigurationManager.ConnectionStrings["CreateConnection"].ConnectionString;
 
                 using (SqlConnection connection = new SqlConnection(conn))
                 {
                     connection.Open();
 
-                    try
+                    foreach (var data in auctionDataList)
                     {
-                        foreach (var data in auctionDataList)
+                        string checkQuery = "SELECT COUNT(*) FROM Auctions WHERE Title = @Title";
+                        using (SqlCommand checkCommand = new SqlCommand(checkQuery, connection))
                         {
-                            // stored procedure
-                            string insertQuery = "SP_InsertIneichenProductData";
+                            checkCommand.Parameters.AddWithValue("@Title", data.Title);
+                            int existingCount = (int)checkCommand.ExecuteScalar();
 
-                            using (SqlCommand command = new SqlCommand(insertQuery, connection))
+                            if (existingCount > 0)
                             {
-                                command.CommandType = CommandType.StoredProcedure;
 
-                                command.Parameters.AddWithValue("@Title", data.Title);
-                                command.Parameters.AddWithValue("@Description", data.Description);
-                                command.Parameters.AddWithValue("@ImageUrl", data.ImageUrl);
-                                command.Parameters.AddWithValue("@Link", data.Link);
-                                command.Parameters.AddWithValue("@LotCount", data.LotCount);
-                                command.Parameters.AddWithValue("@StartDate", data.StartDate);
-                                command.Parameters.AddWithValue("@StartMonth", data.StartMonth);
-                                command.Parameters.AddWithValue("@StartYear", data.StartYear);
-                                command.Parameters.AddWithValue("@StartTime", data.StartTime);
-                                command.Parameters.AddWithValue("@EndDate", data.EndDate);
-                                command.Parameters.AddWithValue("@EndMonth", data.EndMonth);
-                                command.Parameters.AddWithValue("@EndYear", data.EndYear);
-                                command.Parameters.AddWithValue("@EndTime", data.EndTime);
-                                command.Parameters.AddWithValue("@Location", data.Location);
+                                //string updateQuery = @"UPDATE Auctions
+                                //           SET Description = @Description, 
+                                //               ImageUrl = @ImageUrl, 
+                                //               Link = @Link, 
+                                //               LotCount = @LotCount, 
+                                //               StartDate = @StartDate, 
+                                //               StartMonth = @StartMonth, 
+                                //               StartYear = @StartYear, 
+                                //               StartTime = @StartTime, 
+                                //               EndDate = @EndDate, 
+                                //               EndMonth = @EndMonth, 
+                                //               EndYear = @EndYear, 
+                                //               EndTime = @EndTime, 
+                                //               Location = @Location 
+                                //           WHERE Title = @Title";
+                                string updateQuery = "SP_UpdateIneichenProductData";
+                                using (SqlCommand updateCommand = new SqlCommand(updateQuery, connection))
+                                {
+                                    updateCommand.CommandType = CommandType.StoredProcedure;
+                                    updateCommand.Parameters.AddWithValue("@Title", data.Title);
+                                    updateCommand.Parameters.AddWithValue("@Description", data.Description);
+                                    updateCommand.Parameters.AddWithValue("@ImageUrl", data.ImageUrl);
+                                    updateCommand.Parameters.AddWithValue("@Link", data.Link);
+                                    updateCommand.Parameters.AddWithValue("@LotCount", data.LotCount);
+                                    updateCommand.Parameters.AddWithValue("@StartDate", data.StartDate);
+                                    updateCommand.Parameters.AddWithValue("@StartMonth", data.StartMonth);
+                                    updateCommand.Parameters.AddWithValue("@StartYear", data.StartYear);
+                                    updateCommand.Parameters.AddWithValue("@StartTime", data.StartTime);
+                                    updateCommand.Parameters.AddWithValue("@EndDate", data.EndDate);
+                                    updateCommand.Parameters.AddWithValue("@EndMonth", data.EndMonth);
+                                    updateCommand.Parameters.AddWithValue("@EndYear", data.EndYear);
+                                    updateCommand.Parameters.AddWithValue("@EndTime", data.EndTime);
+                                    updateCommand.Parameters.AddWithValue("@Location", data.Location);
 
-                                command.ExecuteNonQuery();
+                                    updateCommand.ExecuteNonQuery();
+                                }
+                                Console.WriteLine("Data Updated Successfully");
+                            }
+                            else
+                            {
+                                string insertQuery = "SP_InsertIneichenProductData";
+
+                                using (SqlCommand insertCommand = new SqlCommand(insertQuery, connection))
+                                {
+                                    insertCommand.CommandType = CommandType.StoredProcedure;
+
+                                    insertCommand.Parameters.AddWithValue("@Title", data.Title);
+                                    insertCommand.Parameters.AddWithValue("@Description", data.Description);
+                                    insertCommand.Parameters.AddWithValue("@ImageUrl", data.ImageUrl);
+                                    insertCommand.Parameters.AddWithValue("@Link", data.Link);
+                                    insertCommand.Parameters.AddWithValue("@LotCount", data.LotCount);
+                                    insertCommand.Parameters.AddWithValue("@StartDate", data.StartDate);
+                                    insertCommand.Parameters.AddWithValue("@StartMonth", data.StartMonth);
+                                    insertCommand.Parameters.AddWithValue("@StartYear", data.StartYear);
+                                    insertCommand.Parameters.AddWithValue("@StartTime", data.StartTime);
+                                    insertCommand.Parameters.AddWithValue("@EndDate", data.EndDate);
+                                    insertCommand.Parameters.AddWithValue("@EndMonth", data.EndMonth);
+                                    insertCommand.Parameters.AddWithValue("@EndYear", data.EndYear);
+                                    insertCommand.Parameters.AddWithValue("@EndTime", data.EndTime);
+                                    insertCommand.Parameters.AddWithValue("@Location", data.Location);
+
+                                    insertCommand.ExecuteNonQuery();
+                                }
+
+                                Console.WriteLine("Data Inserted Successfully");
                             }
                         }
                     }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine(ex.Message.ToString());
-                    }
                 }
+
             }
+
+
+
 
         }
     }
@@ -211,71 +308,3 @@ namespace IneichenScraper
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//foreach (var productHTMLElement in productHTMLElements)
-//{
-//    // scraping logic 
-//    var url = HtmlEntity.DeEntitize(productHTMLElement.QuerySelector("a").Attributes["href"].Value);
-//    var image = HtmlEntity.DeEntitize(productHTMLElement.QuerySelector("img").Attributes["src"].Value);
-//    var name = HtmlEntity.DeEntitize(productHTMLElement.QuerySelector("h2").InnerText);
-//    var price = HtmlEntity.DeEntitize(productHTMLElement.QuerySelector(".price").InnerText);
-//    var pokemonProduct = new PokemonProduct() { Url = url, Image = image, Name = name, Price = price };
-//    pokemonProducts.Add(pokemonProduct);
-//}
-
-
-//var productHTMLElements = currentDocument.DocumentNode.QuerySelectorAll("li.product");
-//// iterating over the list of product HTML elements 
-//foreach (var productHTMLElement in productHTMLElements)
-//{
-//    // scraping logic 
-//    var url = HtmlEntity.DeEntitize(productHTMLElement.QuerySelector("a").Attributes["href"].Value);
-//    var image = HtmlEntity.DeEntitize(productHTMLElement.QuerySelector("img").Attributes["src"].Value);
-//    var name = HtmlEntity.DeEntitize(productHTMLElement.QuerySelector("h2").InnerText);
-//    var price = HtmlEntity.DeEntitize(productHTMLElement.QuerySelector(".price").InnerText);
-//    var pokemonProduct = new PokemonProduct() { Url = url, Image = image, Name = name, Price = price };
-//    pokemonProducts.Add(pokemonProduct);
-//}
